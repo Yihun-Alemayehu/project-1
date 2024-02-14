43343f2257a23da92c8d6c5a6509e887b11e4ffa
Yihun <yankure01@gmail.com>
2024-02-14 21:49:43 +0300

    fdb-01

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_1/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
